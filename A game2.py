c1b0a6a493330ee68ca207a4d331a70c1e40a282
ShAh-321 <mukti007@gmail.com>
2020-06-19 10:47:51 -0400

    Update A game2.py

--- A game2.py
+++ A game2.py
@@ -4,9 +4,19 @@ wn = turtle.Screen()
 wn.title("Snake Game")
 wn.bgcolor("green")
 wn.setup(width=600, height=600)
-wn.tracer(0)
+wn.tracer(0) # turns off the screen updates
 
+#Snake Head
+head = turtle.Turtle()
+head.speed(0) #animation speed of the turtle module
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
 
-
+#Main game loop
+while True:
+    wn.update()
 
 wn.mainloop()
