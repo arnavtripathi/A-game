93ba8c2a408b93145f1436f08368dea1e323a05b
Arnav <mail2arnavtripathi@gmail.com>
2020-06-22 11:25:39 -0400

    Update A game2.py

--- A game2.py
+++ A game2.py
@@ -16,16 +16,43 @@ head.shape("square")
 head.color("black")
 head.penup()
 head.goto(0,0)
-head.direction = "up"
+head.direction = "stop"
 
 #Functions
+def go_left():
+    head.direction = "left"
+
+def go_right():
+    head.direction = "right"
+
+def go_up():
+    head.direction = "up"
+
+def go_down():
+    head.direction = "down"
+
 def move():
     if head.direction == "up":
         y = head.ycor()
         head.sety(y + 20)
 
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
 
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
 
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+#Keyboard Bindings
+wn.listen()
+wn.onkeypress(go_up, "Up")
+wn.onkeypress(go_down, "Down")
+wn.onkeypress(go_left, "Left")
+wn.onkeypress(go_right, "Right")
 
 #Main game loop
 while True:
