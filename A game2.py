4b181d67211b3d8e0a5ad207a26757e821ab28fa
ShAh-321 <mukti007@gmail.com>
2020-06-26 12:38:56 -0400

    Update A game2.py

--- A game2.py
+++ A game2.py
@@ -4,6 +4,11 @@ import random
 
 delay = 0.1
 
+#Score
+score = 0#0 means that that is what u start with in the begining. 
+high_score = 0
+
+
 wn = turtle.Screen()
 wn.title("Snake Game")
 wn.bgcolor("green")
@@ -30,18 +35,32 @@ food.goto(0,100)
 
 segments = []
 
+#Pen
+pen =  turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Score: 0   High Score: 0", align="center", font=("Courier", 24, "italic"))
+
 #Functions
 def go_left():
-    head.direction = "left"
+    if head.direction != "right":
+        head.direction = "left"
 
 def go_right():
-    head.direction = "right"
+    if head.direction != "left":
+        head.direction = "right"
 
 def go_up():
-    head.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 
 def go_down():
-    head.direction = "down"
+    if head.direction != "up":
+        head.direction = "down"
 
 def move():
     if head.direction == "up":
@@ -70,24 +89,59 @@ wn.onkeypress(go_right, "Right")
 while True:
     wn.update()
 
+    #Check for collisons with border
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+
+        #Hide the segmens when u lose
+        for segment in segments:
+            segment.goto(1000, 1000 )
+
+        #Clear the segments list which clears all segments after u lose
+        segments.clear()
+
+        #Reset the score when u die
+        score = 0
+        delay = 0.1
+
+        pen.clear()
+        pen.write("Score:{}" "  High Score: {}".format(score, high_score),align="center", font=("Courier", 24, "italic")) 
+            
+    #Check for collisions with food
     if head.distance(food) < 20:
         x = random.randint(-290, 290)
         y = random.randint(-290, 290)
         food.goto(x, y)
-
+    #Add a segment
         new_segment = turtle.Turtle()
-        new_segment.speed(100)
-        new_segment.penup()
-        new_segment.color("orange")
+        new_segment.speed(0)
         new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
         segments.append(new_segment)
 
-    #Move the end segemnts in reverse order
+    #Shorten the delay
+        delay -= 0.001
+
+    #Increase the score
+        score += 10
+
+        if score > high_score:
+            high_score = score
+
+        pen.clear()
+        pen.write("Score:{}" "  High Score: {}".format(score, high_score),align="center", font=("Courier", 24, "italic")) 
+            
+    #Move the end segemnts first in reverse order
     for index in range(len(segments)-1, 0, -1):
         x = segments[index - 1].xcor()
         y = segments[index - 1].ycor()
         segments[index].goto(x, y)
 
+    #Move segment 0 to where the head is
         if len(segments) > 0:
             x = head.xcor()
             y = head.ycor()
@@ -96,6 +150,38 @@ while True:
         
     move()
 
+    #Check for body collisions
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+
+
+            for segment in segments:
+                segment.goto(1000, 1000)
+
+
+
+
+         #Clear the segments list which clears all segments after u lose
+            segments.clear()
+
+            #Reset the score when u die
+            score = 0
+
+            delay = 0.1
+            
+
+
+        #Hide the segments when u lose
+            for segment in segments:
+                segment.goto(1000, 1000 )
+
+        #Clear the segments list which clears all segments after u lose
+            segments.clear()
+
+        
     time.sleep(delay)
 
 wn.mainloop()
